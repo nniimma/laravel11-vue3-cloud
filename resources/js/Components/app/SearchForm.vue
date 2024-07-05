60b835dbcb4e22540fb42ddc82470564b8e59e8e
Nima <alireza.khazforoosh@adventistas.org>
2024-07-05 14:46:08 -0300

    useForm inside inertia

--- resources/js/Components/app/SearchForm.vue
+++ resources/js/Components/app/SearchForm.vue
@@ -1,12 +1,22 @@
 <template>
-<div>search</div>
+    <form class="w-[600px] h-[80px] flex items-center">
+        <text-input 
+            class="block w-full mr-2" 
+            type="text" 
+            v-model="form.search" 
+            autocomplete 
+            placeholder="Search for files and folders"/>
+    </form>
 </template>
 
 <script setup>
     // Imports
-
+    import TextInput from '@/Components/TextInput.vue';
+    import { useForm } from '@inertiajs/vue3';
     // Uses
-
+    const form = useForm({
+        search: ''
+    })
     // Refs
 
     // Props & Emits
