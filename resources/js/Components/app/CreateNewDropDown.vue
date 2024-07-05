c2b2d629eb9d3e5fa18646b4f785ede6508ab771
Nima <alireza.khazforoosh@adventistas.org>
2024-07-05 14:56:19 -0300

    dropdown menu from headless ui and install heroIcons -https://headlessui.com/ - npm install @headlessui/vue@^1.7 -npm install @heroicons/vue

---
+++ resources/js/Components/app/CreateNewDropDown.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script setup>
+    // Imports
+
+    // Uses
+
+    // Refs
+
+    // Props & Emits
+
+    // Computed
+
+    // Methods
+
+    // Hooks
+
+    
+</script>
+
+<style scoped>
+    
+</style>
