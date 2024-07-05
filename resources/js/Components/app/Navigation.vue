e281237a2a131eb0dd0aa5573458989ff0818af7
Nima <alireza.khazforoosh@adventistas.org>
2024-07-05 14:20:37 -0300

    using link of inerita: -<Link :href="route('dashboard')">

--- resources/js/Components/app/Navigation.vue
+++ resources/js/Components/app/Navigation.vue
@@ -1,9 +1,19 @@
 <template>
-<div>navigation</div>
+    <nav class="min-w-[200px]">
+        <div class="h-[80px] px-3 flex items-center gap-3">
+            <Link :href="route('dashboard')">
+            <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+            </Link>
+        </div>
+    </nav>
 </template>
 
 <script setup>
     // Imports
+    import {
+        Link
+    } from '@inertiajs/vue3';
+    import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 
     // Uses
 
