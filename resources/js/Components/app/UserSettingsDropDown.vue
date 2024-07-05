0084c861c4f5379a76df8086eed3eadee0b2302d
Nima <alireza.khazforoosh@adventistas.org>
2024-07-05 14:59:15 -0300

    getting the user name inside inertia: -{{ $page.props.auth.user.name }}

--- resources/js/Components/app/UserSettingsDropDown.vue
+++ resources/js/Components/app/UserSettingsDropDown.vue
@@ -1,10 +1,9 @@
 <template>
-    <div class="fixed top-16 w-56 text-right">
         <Menu as="div" class="relative inline-block text-left">
             <div>
                 <MenuButton
                     class="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-                    Options
+                    {{ $page.props.auth.user.name }}
                     <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
                         aria-hidden="true" />
                 </MenuButton>
@@ -39,47 +38,9 @@
                         </button>
                         </MenuItem>
                     </div>
-                    <div class="px-1 py-1">
-                        <MenuItem v-slot="{ active }">
-                        <button
-                            :class="[
-                                active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                            ]">
-                            <ArchiveIcon :active="active" class="mr-2 h-5 w-5 text-violet-400"
-                                aria-hidden="true" />
-                            Archive
-                        </button>
-                        </MenuItem>
-                        <MenuItem v-slot="{ active }">
-                        <button
-                            :class="[
-                                active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                            ]">
-                            <MoveIcon :active="active" class="mr-2 h-5 w-5 text-violet-400" aria-hidden="true" />
-                            Move
-                        </button>
-                        </MenuItem>
-                    </div>
-
-                    <div class="px-1 py-1">
-                        <MenuItem v-slot="{ active }">
-                        <button
-                            :class="[
-                                active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                            ]">
-                            <DeleteIcon :active="active" class="mr-2 h-5 w-5 text-violet-400"
-                                aria-hidden="true" />
-                            Delete
-                        </button>
-                        </MenuItem>
-                    </div>
                 </MenuItems>
             </transition>
         </Menu>
-    </div>
 </template>
 
 
