143200a9cd3d707e1833619a644cf7714be35731
Nima <alireza.khazforoosh@adventistas.org>
2024-07-05 14:39:48 -0300

    using NavLink of inetia and active inside it: -<nav-link :href="route('dashboard')" :active="$page.url == '/dashboard'">My Files</nav-link>

--- resources/js/Components/app/Navigation.vue
+++ resources/js/Components/app/Navigation.vue
@@ -1,19 +1,28 @@
 <template>
     <nav class="min-w-[200px]">
-        <div class="h-[80px] px-3 flex items-center gap-3">
-            <Link :href="route('dashboard')">
+            <Link :href="route('dashboard')" class="h-[80px] px-3 flex items-center gap-3">
             <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+            LaraStorage
             </Link>
-        </div>
+         <div class="px-3">
+            <create-new-drop-down/>
+
+            <div class="py-3">
+                <nav-link :href="route('dashboard')" :active="$page.url == '/dashboard'">My Files</nav-link>
+                <nav-link href="/">Shared with me</nav-link>
+                <nav-link href="/">Shared by me</nav-link>
+                <nav-link href="/">Trash</nav-link>
+            </div>
+         </div>
     </nav>
 </template>
 
 <script setup>
     // Imports
-    import {
-        Link
-    } from '@inertiajs/vue3';
+    import { Link } from '@inertiajs/vue3';
     import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+    import NavLink from '@/Components/NavLink.vue';
+    import CreateNewDropDown from '@/Components/app/CreateNewDropDown.vue';
 
     // Uses
 
