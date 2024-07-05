5399b5dd2876b0bd83db75dfd86920b21f000745
Nima <alireza.khazforoosh@adventistas.org>
2024-07-05 15:15:56 -0300

    setting dropdown menu style

--- resources/js/Components/app/UserSettingsDropDown.vue
+++ resources/js/Components/app/UserSettingsDropDown.vue
@@ -2,9 +2,9 @@
         <Menu as="div" class="relative inline-block text-left">
             <div>
                 <MenuButton
-                    class="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+                    class="inline-flex w-full justify-center rounded-md 0 px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
                     {{ $page.props.auth.user.name }}
-                    <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
+                    <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5 text-gray-800"
                         aria-hidden="true" />
                 </MenuButton>
             </div>
@@ -14,31 +14,27 @@
                 leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100"
                 leave-to-class="transform scale-95 opacity-0">
                 <MenuItems
-                    class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-                    <div class="px-1 py-1">
-                        <MenuItem v-slot="{ active }">
-                        <button
-                            :class="[
-                                active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                            ]">
-                            <EditIcon :active="active" class="mr-2 h-5 w-5 text-violet-400" aria-hidden="true" />
-                            Edit
-                        </button>
-                        </MenuItem>
-                        <MenuItem v-slot="{ active }">
-                        <button
-                            :class="[
-                                active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                            ]">
-                            <DuplicateIcon :active="active" class="mr-2 h-5 w-5 text-violet-400"
-                                aria-hidden="true" />
-                            Duplicate
-                        </button>
-                        </MenuItem>
-                    </div>
-                </MenuItems>
+                class="absolute right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                    <ResponsiveNavLink class="flex cursor-pointer" :href="route('profile.edit')"
+                        :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">
+
+                        <UserIcon  class="size-4 mr-2" aria-hidden="true" />
+                        Profile
+                    </ResponsiveNavLink>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                    <ResponsiveNavLink class="flex cursor-pointer" :href="route('logout')" method="post" as="button"
+                        :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">
+
+                        <ArrowRightOnRectangleIcon  class="size-4 mr-2" aria-hidden="true" />
+
+                        Logout
+                    </ResponsiveNavLink >
+                    </MenuItem>
+                </div>
+            </MenuItems>
             </transition>
         </Menu>
 </template>
@@ -54,7 +50,9 @@
         MenuItems,
         MenuItem
     } from '@headlessui/vue'
-    import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+    import { ChevronDownIcon, UserIcon, ArrowRightOnRectangleIcon   } from '@heroicons/vue/20/solid';
+    import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+
     // Uses
 
     // Refs
