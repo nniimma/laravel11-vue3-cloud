a808d45b9fcc16a22d4ff75cfbe833c4f7517e81
Nima <alireza.khazforoosh@adventistas.org>
2024-07-10 14:21:40 -0300

    update

--- resources/js/Components/Files/DeleteFilesButton.vue
+++ resources/js/Components/Files/DeleteFilesButton.vue
@@ -1,9 +1,9 @@
 <template>
-    <button @click="onDeleteClick"
-        class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+    <danger-button @click="onDeleteClick"
+        class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900  border border-gray-200 rounded-lg hover:bg-red-700 hover:bg-gray-900 focus:z-10 focus:ring-2 focus:ring-black focus:text-black">
         <TrashIcon class="h-4 w-4 mr-2" />
         Delete
-    </button>
+    </danger-button>
 
     <confirmation-dialog :show="showDeleteDialog" message="Are you sure you want to delete the selected files?"
         @cancle="onDeleteCancle" @confirm="onDeleteConfirm" />
@@ -12,7 +12,8 @@
 <script setup>
     // Imports
     import ConfirmationDialog from '@/Components/ConfirmationDialog.vue'
-import { showErrorDialog } from '@/event-bus';
+    import DangerButton from '@/Components/DangerButton.vue'
+    import { showErrorDialog } from '@/event-bus';
     import {
         TrashIcon
     } from '@heroicons/vue/20/solid';
