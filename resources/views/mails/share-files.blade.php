ee69c5d0bb4033a36f5741ee4b87f479b93a14ad
Nima <alireza.khazforoosh@adventistas.org>
2024-07-15 11:01:42 -0300

    update mail content

--- resources/views/mails/share-files.blade.php
+++ resources/views/mails/share-files.blade.php
@@ -1,6 +1,6 @@
-<h2>Hello <b>{{ $user->name }}</b></h2>
+<h2>Hello <u>{{ $user->name }}</u></h2>
 
-<h4>User <b>{{ $author->name }}</b> share the following files to you.</h4>
+<h4>User <u>{{ $author->name }}</u> share the following files to you.</h4>
 <hr>
 @foreach ($files as $file)
     <p>{{ $file->is_folder ? 'Folder: ' : 'File: ' }}{{ $file->name }}</p>
