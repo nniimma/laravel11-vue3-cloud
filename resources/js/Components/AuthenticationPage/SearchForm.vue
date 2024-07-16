53c0af4ee27589087e98d90bca98bd9120f2766a
Nima <alireza.khazforoosh@adventistas.org>
2024-07-16 10:24:23 -0300

    sending the text to url for query both favorites and search

--- resources/js/Components/AuthenticationPage/SearchForm.vue
+++ resources/js/Components/AuthenticationPage/SearchForm.vue
@@ -1,35 +1,39 @@
 <template>
-    <form class="w-[600px] h-[80px] flex items-center">
-        <text-input 
-            class="block w-full mr-2" 
-            type="text" 
-            v-model="form.search" 
-            autocomplete 
-            placeholder="Search for files and folders"/>
-    </form>
+    <div class="w-[600px] h-[80px] flex items-center">
+        <text-input class="block w-full mr-2" type="text" v-model="search" autocomplete @keyup.enter.prevent="onSearch"
+            placeholder="Search for files and folders" />
+    </div>
 </template>
 
 <script setup>
     // Imports
     import TextInput from '@/Components/TextInput.vue';
-    import { useForm } from '@inertiajs/vue3';
+    import {
+        router,
+        useForm
+    } from '@inertiajs/vue3';
+    import {
+        ref
+    } from 'vue';
     // Uses
-    const form = useForm({
-        search: ''
-    })
+
     // Refs
+    const search = ref('')
 
     // Props & Emits
 
     // Computed
 
     // Methods
+    function onSearch() {
+        const params = new URLSearchParams(window.location.search)
+        params.set('search', search.value)
+        router.get(window.location.pathname + '?' + params.toString())
+    }
 
     // Hooks
-
-    
 </script>
 
 <style scoped>
-    
+
 </style>
